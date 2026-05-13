e259f4da074a5e3393c92a7cdb783077896720a4
Abdullah7772 <abddullah.butt123@gmail.com>
2026-05-13 19:35:28 +0500

    update 1

--- resources/js/Layouts/NonEmbedded/GuestLayout.jsx
+++ resources/js/Layouts/NonEmbedded/GuestLayout.jsx
@@ -1,33 +1,32 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import {
-    Grid2
-} from '@mui/material';
-import MainLogo from '@/Images/MainLogo.svg';
+// import ApplicationLogo from '@/Components/ApplicationLogo';
+// import { Link } from '@inertiajs/react';
+// import {
+//     Grid2
+// } from '@mui/material';
 
 
-export default function GuestLayout({ children }) {
-    return (
-        <div className="min-h-screen  items-center bg-white  sm:justify-center sm:pt-0">
-            <Grid2 container spacing={3} height={'100dvh'} alignItems="center">
-                <Grid2 size={{ xs: 12, md: 6 }} height={'100%'}  sx={{background:'#023F80', display: 'flex', justifyContent: 'center', alignItems: 'center', display:{ xs: 'none', md: 'flex' }}}>
+// export default function GuestLayout({ children }) {
+//     return (
+//         <div className="min-h-screen  items-center bg-white  sm:justify-center sm:pt-0">
+//             <Grid2 container spacing={3} height={'100dvh'} alignItems="center">
+//                 <Grid2 size={{ xs: 12, md: 6 }} height={'100%'}  sx={{background:'#023F80', display: 'flex', justifyContent: 'center', alignItems: 'center', display:{ xs: 'none', md: 'flex' }}}>
 
-                    <Link href="/">
-                    <img src={MainLogo} className='w-60' alt="Frago Logo" />
-                        {/* <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" /> */}
-                    </Link>
+//                     <Link href="/">
+//                     {/* <img src={MainLogo} className='w-60' alt="Frago Logo" /> */}
+//                         {/* <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" /> */}
+//                     </Link>
 
-                </Grid2>
+//                 </Grid2>
 
-                <Grid2 size={{ xs: 12, md: 6 }} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+//                 <Grid2 size={{ xs: 12, md: 6 }} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 
-                    <div className="mt-6 w-full overflow-hidden px-6   sm:max-w-md sm:rounded-lg">
-                        {children}
-                    </div>
+//                     <div className="mt-6 w-full overflow-hidden px-6   sm:max-w-md sm:rounded-lg">
+//                         {children}
+//                     </div>
 
 
-                </Grid2>
-            </Grid2>
-        </div>
-    );
-}
+//                 </Grid2>
+//             </Grid2>
+//         </div>
+//     );
+// }
